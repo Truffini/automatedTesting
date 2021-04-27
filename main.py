8dde0b4355d25eb868120199dbbf6da293afda21
Trevor <truffini@student.bridgew.edu>
2021-04-27 02:33:09 -0400

    adds tests comments out user input functions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Wic_Eligible_food = {
 }
 clothing = {
     'shirt': 15.00, 'shoes': 100.00, 'pants': 20.00, 'shorts': 10.00, 'jacket': 50.00, 'hat': 30.00,
-    'socks': 5.00, 'underwear': 10.00, 'sweatshirt': 40.00, 'checkout': 0
+    'socks': 5.00, 'underwear': 10.00, 'sweatshirt': 40.00, 'gucci': 180, 'checkout': 0
 }
 everything_else = {'toys': 9.99, 'tools': 19.99, "school supplies": 29.99, "appliances": 14.99, "chair": 24.99,
                    'laptop': 999.99, 'checkout': 0
@@ -18,9 +18,9 @@ everything_else = {'toys': 9.99, 'tools': 19.99, "school supplies": 29.99, "appl
 # print("Everything else ", everything_else)
 
 
-def get_state(sales_tax):
-    state = 'MA'
-    #  state = input('enter state abbreviation - ')
+def get_state(state):
+    # state = 'MA'
+    # state = input('enter state abbreviation - ')
     print(state)
     if state == 'MA':
         sales_tax = .0625
@@ -32,21 +32,22 @@ def get_state(sales_tax):
         print('not available')
 
     print("your sales tax will be", sales_tax)
-    return state, sales_tax
+    return sales_tax
 
 
 def get_wic(Wic_item):
-    print("type checkout to move on to clothing")
-    Wic_item = input("add Wic item - ")
+    # print("type checkout to move on to clothing")
+    # Wic_item = input("add Wic item - ")
     price_of_Wic_item = Wic_Eligible_food.get(Wic_item)
-    print(price_of_Wic_item)
+    # print(price_of_Wic_item)
     list_of_Wic = []
 
-    while Wic_item != 'checkout':
+    if Wic_item != 'checkout':
         list_of_Wic.append(price_of_Wic_item)
-        Wic_item = input("add Wic item - ")
+        # Wic_item = input("add Wic item - ")
         price_of_Wic_item = Wic_Eligible_food.get(Wic_item)
         print(price_of_Wic_item)
+        return price_of_Wic_item
 
     if Wic_item == 'checkout':
         print(list_of_Wic)
@@ -57,18 +58,19 @@ def get_wic(Wic_item):
         return Wic_total
 
 
-def get_clothing():
-    print("type checkout to move to everything else")
-    clothing_item = input("add clothing item - ")
+def get_clothing(clothing_item):
+    # print("type checkout to move to everything else")
+    # clothing_item = input("add clothing item - ")
     price_of_clothing_item = clothing.get(clothing_item)
-    print(price_of_clothing_item)
+    # print(price_of_clothing_item)
     list_of_clothing = []
 
     while clothing_item != 'checkout':
         list_of_clothing.append(price_of_clothing_item)
-        clothing_item = input("add clothing item - ")
+        # clothing_item = input("add clothing item - ")
         price_of_clothing_item = clothing.get(clothing_item)
         print(price_of_clothing_item)
+        return price_of_clothing_item
 
     if clothing_item == 'checkout':
         print(list_of_clothing)
@@ -79,17 +81,19 @@ def get_clothing():
         return clothing_total
 
 
-def get_everything_else():
-    print("type checkout to finish")
-    everything_else_item = input("add other item - ")
+def get_everything_else(everything_else_item):
+    # print("type checkout to finish")
+    # everything_else_item = input("add other item - ")
     price_of_everything_else_item = everything_else.get(everything_else_item)
-    print(price_of_everything_else_item)
+    # print(price_of_everything_else_item)
     list_of_everything_else = []
+
     while everything_else_item != 'checkout':
         list_of_everything_else.append(price_of_everything_else_item)
-        everything_else_item = input("add  other item - ")
+        # everything_else_item = input("add  other item - ")
         price_of_everything_else_item = everything_else.get(everything_else_item)
         print(price_of_everything_else_item)
+        return price_of_everything_else_item
 
     if everything_else_item == 'checkout':
         print(list_of_everything_else)
@@ -100,25 +104,38 @@ def get_everything_else():
         return everything_else_total
 
 
-def get_total(Wic_total, clothing_total, everything_else_total, sales_tax, state):
-    if state == 'MA':
+def get_total_sales_tax(clothing_total, everything_else_total, sales_tax, state):
+    if state == 'MA' and clothing_total > 175:
+        sales_tax_total = (clothing_total + everything_else_total) * sales_tax
+        print("sales tax total - ", sales_tax_total)
+        return sales_tax_total
+    elif state == 'MA' and clothing_total < 175:
         sales_tax_total = everything_else_total * sales_tax
         print("sales tax total - ", sales_tax_total)
-        print("your total including tax is - ", (Wic_total + clothing_total + everything_else_total + sales_tax_total))
+        return sales_tax_total
     else:
         sales_tax_total = (clothing_total + everything_else_total) * sales_tax
         print("sales tax total - ", sales_tax_total)
-        print("your total including tax is - ", (Wic_total + clothing_total + everything_else_total + sales_tax_total))
+        return sales_tax_total
+
+
+def goods_total(Wic_total, clothing_total, everything_else_total):
+    goods_total = Wic_total + clothing_total + everything_else_total
+    print("total before tax", goods_total)
+    return goods_total
 
 
 def main():
-    state, sales_tax = get_state()
-    Wic_total = get_wic()
-    clothing_total = get_clothing()
-    everything_else_total = get_everything_else()
-    get_total(Wic_total, clothing_total, everything_else_total, sales_tax, state)
+    state = 'MA'
+    sales_tax = get_state(state)
+    Wic_total = get_wic('Fish')
+    clothing_total = get_clothing('gucci')
+    everything_else_total = get_everything_else('tools')
+    total_after_tax = (get_total_sales_tax(clothing_total, everything_else_total, sales_tax, state) +
+                       goods_total(Wic_total, clothing_total, everything_else_total))
+    print("total after tax", total_after_tax)
+    return total_after_tax
 
 
 if __name__ == '__main__':
     main()
-
--- test_main.py
+++ test_main.py
@@ -1,19 +1,83 @@
 import main
-import pytest
 
 
-def test_sales_tax():
-    x = main.get_state()
-    print(x)
-    assert x == 4
+def test_sales_tax_ma():
+    sales_tax = main.get_state("MA")
+    assert sales_tax == .0625
 
 
-def test_Wic_total():
-    y = main.get_wic()
-    print(y)
-    assert y == 23
+def test_sales_tax_me():
+    sales_tax = main.get_state("ME")
+    assert sales_tax == .055
 
 
-def test():
-    print("test")
+def test_sales_tax_nh():
+    sales_tax = main.get_state("NH")
+    assert sales_tax == 0
 
+
+def test_wic_total():
+    wic_item_price = main.get_wic('Fish')
+    assert wic_item_price == main.Wic_Eligible_food.get('Fish')
+
+
+def test_clothing_total():
+    clothing_item_price = main.get_clothing('hat')
+    assert clothing_item_price == main.clothing.get('hat')
+
+
+def test_everything_else_total():
+    everything_else_item_price = main.get_everything_else('toys')
+    assert everything_else_item_price == main.everything_else.get('toys')
+
+
+def test_get_sales_tax_nh():
+    sales_tax = main.get_state('NH')
+    clothing = main.get_clothing('gucci')
+    everything_else = main.get_everything_else('toys')
+    sales_tax_total = main.get_total_sales_tax(clothing, everything_else, sales_tax, 'NH')
+    assert sales_tax_total == 0
+
+
+def test_get_sales_tax_ma():
+    sales_tax = main.get_state('MA')
+    clothing = main.get_clothing('gucci')
+    everything_else = main.get_everything_else('tools')
+    if clothing > 175:
+        sales_tax_total = main.get_total_sales_tax(clothing, everything_else, sales_tax, 'MA')
+        assert sales_tax_total == (clothing + everything_else) * sales_tax
+    else:
+        sales_tax_total = main.get_total_sales_tax(clothing, everything_else, sales_tax, 'MA')
+        assert sales_tax_total == everything_else * sales_tax
+
+
+def test_get_sales_tax_me():
+    sales_tax = main.get_state('ME')
+    clothing = main.get_clothing('hat')
+    everything_else = main.get_everything_else('chair')
+    sales_tax_total = main.get_total_sales_tax(clothing, everything_else, sales_tax, 'ME')
+    assert sales_tax_total == (clothing + everything_else) * sales_tax
+
+
+def test_get_total_with_tax():
+    state = 'ME'
+    sales_tax = main.get_state(state)
+    clothing = main.get_clothing('hat')
+    everything_else = main.get_everything_else('chair')
+    Wic_eligible_food = main.get_wic('Fish')
+    sales_tax_total = main.get_total_sales_tax(clothing, everything_else, sales_tax, state)
+    final = main.goods_total(sales_tax_total + Wic_eligible_food, clothing, everything_else)
+    assert final == sales_tax_total + clothing + Wic_eligible_food + everything_else
+
+
+def test_get_total_without_tax():
+    clothing = main.get_clothing('hat')
+    everything_else = main.get_everything_else('chair')
+    Wic_eligible_food = main.get_wic('Fish')
+    total_without_tax = main.goods_total(Wic_eligible_food, clothing, everything_else)
+    assert total_without_tax == clothing + Wic_eligible_food + everything_else
+
+
+def test_main():
+    total_after_tax = main.main()
+    assert total_after_tax == 223.489375  # only checks if it get the right total for fish, gucci and tools in MA
